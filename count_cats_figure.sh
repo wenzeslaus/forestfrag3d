ea61d355f2dbdb3c0ff5139b416eb69395a5fb25
Vaclav Petras <wenzeslaus@gmail.com>
2016-11-23 23:27:48 -0500

    use more strict vector grid, instead of d.grid

--- count_cats_figure.sh
+++ count_cats_figure.sh
@@ -5,17 +5,6 @@ WIND_OVERRIDE=count_category_explanation_region
 g.region -u save=$WIND_OVERRIDE
 export WIND_OVERRIDE
 
-# set region to the raster and one cell bellow
-g.region s=-1 n=7 w=0 e=7 res=1
-
-export GRASS_FONT=sans
-eval `g.region -g`
-
-DESIRED_WIDTH=300
-DESIRED_HEIGHT=`python -c "print $DESIRED_WIDTH / float($cols) * $rows"`
-
-FONT_SIZE=7
-
 r.in.ascii input=- output=count_categories_explanation_cells <<EOF
 north: 7
 south: 0
@@ -32,9 +21,23 @@ cols: 7
 * * * * * * 1
 EOF
 
-r.colors map=count_categories_explanation_cells rules=- <<EOF
-1 #ff9200
-EOF
+# set region to the raster and one cell bellow
+g.region raster=count_categories_explanation_cells
+
+# create grid which only overlaps the region
+v.mkgrid map=count_categories_explanation_grid position=region
+
+# set region to the raster and one cell bellow
+g.region s=s-1
+
+export GRASS_FONT=sans
+eval `g.region -g`
+
+DESIRED_WIDTH=300
+DESIRED_HEIGHT=`python -c "print $DESIRED_WIDTH / float($cols) * $rows"`
+
+FONT_SIZE=7
+GRID_WIDTH=1
 
 v.in.ascii input=- format=standard -n output=count_categories_explanation_line <<EOF
 L 7 1
@@ -48,18 +51,15 @@ L 7 1
  1 1
 EOF
 
+r.colors map=count_categories_explanation_cells rules=- <<EOF
+1 #ff9200
+EOF
+
 d.mon start=cairo output=count_categories.png width=$DESIRED_WIDTH height=$DESIRED_HEIGHT
 d.erase  # previous image is not cleaned
 d.rast map=count_categories_explanation_cells
-d.grid -b -t size=1 color=black
-d.graph -m <<EOF
-color white
-polygon
-    -10 -1
-    70 -1
-    70 0
-    -10 0
-EOF
+d.vect map=count_categories_explanation_grid \
+    color=26:26:26 fill_color=none width=$GRID_WIDTH
 # TODO: replace by geographical coordinates
 d.text text="1" at=7.3,7.3 align=cc color=black size=$FONT_SIZE
 d.text text="2" at=21.6,7.3 align=cc color=black size=$FONT_SIZE
@@ -73,16 +73,9 @@ d.mon stop=cairo
 d.mon start=cairo output=count_categories_surface.png width=$DESIRED_WIDTH height=$DESIRED_HEIGHT
 d.erase  # previous image is not cleaned
 d.rast map=count_categories_explanation_cells
-d.grid -b -t size=1 color=black
+d.vect map=count_categories_explanation_grid \
+    color=26:26:26 fill_color=none width=$GRID_WIDTH
 d.vect map=count_categories_explanation_line color=0:29:90 width=8
-d.graph -m <<EOF
-color white
-polygon
-    0 -1
-    7 -1
-    7 0
-    0 0
-EOF
 # TODO: replace by geographical coordinates
 d.text text="1" at=7.3,7.3 align=cc color=black size=$FONT_SIZE
 d.text text="2" at=21.6,7.3 align=cc color=black size=$FONT_SIZE
