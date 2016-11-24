65c48b8568a1984389e3dc728e4eef2acf74fe22
Vaclav Petras <wenzeslaus@gmail.com>
2016-11-24 11:09:45 -0500

    less contours and smoother

--- contours.sh
+++ contours.sh
@@ -2,6 +2,7 @@
 
 r.relief input=ground output=relief
 
-#r.contour -t --overwrite input=ground output=contours_full step=8.2021 cut=200
-r.contour -t --overwrite input=ground output=contours_full step=3 cut=2
+r.neighbors input=ground output=ground_smooth method=average size=21
+
+r.contour -t --overwrite input=ground_smooth output=contours_full step=8 cut=200
 v.generalize --overwrite input=contours_full output=contours method=boyle threshold=500
